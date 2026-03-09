a280f0c48cd2175eeae7b4c0bb16962d630c0af4
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2026-03-09 02:51:03 -0500

    actualizacion fixes

--- lib/presentation/providers/personality_provider.dart
+++ lib/presentation/providers/personality_provider.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../data/models/personality.dart';
 import '../../data/services/personality_service.dart';
 import 'api_provider.dart';
+import 'auth_provider.dart';
 
 final personalityServiceProvider = Provider<PersonalityService>((ref) {
   final apiService = ref.watch(apiServiceProvider);
@@ -11,6 +12,10 @@ final personalityServiceProvider = Provider<PersonalityService>((ref) {
 });
 
 final personalitiesProvider = FutureProvider<List<Personality>>((ref) async {
+  final user = ref.watch(authProvider).value;
+  if (user == null) {
+    return [];
+  }
   final service = ref.watch(personalityServiceProvider);
   return service.getPersonalities();
 });
